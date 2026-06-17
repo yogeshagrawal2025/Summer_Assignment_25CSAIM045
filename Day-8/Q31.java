5733885c8df37ea968863793353e5e54b228f47f
Yogesh Agrawal <yogesh.agrawal2025@glbajajgroup.org>
2026-06-17 11:22:20 +0530

    Day-8 uploaded

---
+++ Day-8/Q31.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Q31 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            char ch = 'A';
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();   
+        }
+        scanner.close();    
+    }
+}
---
+++ Day-8/Q32.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Q32 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();   
+        }
+        scanner.close();    
+    }
+}
